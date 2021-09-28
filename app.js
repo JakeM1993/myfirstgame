960b77cc6f5255af3456afdd4e0b757aeb88a36b
JakeM1993 <jake.mulraney@gmail.com>
2021-09-28 15:26:46 +0100

    currently working on adding the letter clicked to empty array

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const letter_buttons = document.querySelector(".letter__buttons");
 const hidden__word = document.querySelector(".hidden__word");
 const hidden__letter = document.querySelectorAll(".hidden__letter");
 
+const hidden__letters = ["T", "E", "S", "T", "I", "N", "G"];
+const letterGuess = []
 
 newGame__button.addEventListener("click", () => {
     letter_buttons.style.display = "grid";
@@ -11,7 +13,18 @@ newGame__button.addEventListener("click", () => {
     hidden__word.style.display = "flex";
 })
 
-
-
-// hidden__letter.classList.add('revealed__letter');
-// hidden__letter.classList.remove('hidden__letter');
+letter_buttons.addEventListener("click", (event) => {
+    letterValue = event.target.innerText
+    console.log(hidden__letters[letterValue])
+    hidden__letters.split("").map(letter => 
+        {
+        letter.push()
+        const letterGuess = (letterValue) 
+        console.log(letter [letterValue])
+    })
+    if (letterGuess == hidden__letters[i]) {
+    hidden__letter.classList.add('revealed__letter');
+    hidden__letter.classList.remove('hidden__letter');
+    } else { 
+    }
+})
