45695d5ddf2b1dbfaa61909f6da6d2ba864b2dea
JakeM1993 <jake.mulraney@gmail.com>
2021-09-28 08:48:20 +0100

    hidden the hidden letters

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const newGame__button = document.querySelector(".newGame__button");
 const options__button = document.querySelector(".options__button");
 const letter_buttons = document.querySelector(".letter__buttons");
+const hidden__word = document.querySelector(".hidden__word");
+const hidden__letter = document.querySelectorAll(".hidden__letter");
 
 
 newGame__button.addEventListener("click", () => {
     letter_buttons.style.display = "grid";
     newGame__button.style.display = "none"; 
-    options__button.style.display = "none";
+    hidden__word.style.display = "flex";
 })
+
+
+
+// hidden__letter.classList.add('revealed__letter');
+// hidden__letter.classList.remove('hidden__letter');
